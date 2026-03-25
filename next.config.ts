b191014fa1f5b6103d420b2e7f4aa5ee6c52ff5f
Rana Ahsan Ansar <87710416+ranaahsanansar@users.noreply.github.com>
2026-03-25 18:48:56 +0500

    Fixes

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,15 @@ const nextConfig: NextConfig = {
   images: {
     formats: ["image/avif", "image/webp"],
     minimumCacheTTL: 86400,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+        port: "",
+        pathname: "/photos/**",
+        search: "",
+      },
+    ],
   },
 };
 
